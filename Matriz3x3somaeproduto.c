a5ddac870b9f8cb59a8f6253fdbb5894e99345c3
Pedro <34948480+pdrhp@users.noreply.github.com>
2023-03-30 11:32:45 -0300

    Add files via upload

--- Matriz3x3somaeproduto.c
+++ Matriz3x3somaeproduto.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    int matriz[3][3], i, j;
-    int soma = 0, produto = 1;
+  int matriz[3][3], y, x;
+  int soma = 0;
+  int produto = 1;
 
-    printf("Digite os 9 números inteiros em ordem crescente dos índices:\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            scanf("%d", &matriz[i][j]);
-            if (j > i) {
-                soma += matriz[i][j];
-            }
-            if (j < i) {
-                produto *= matriz[i][j];
-            }
-        }
+  printf("Insira os 9 numeros da matriz 3x3: ");
+  for(y = 0; y < 3 ; y++)
+    for(x = 0; x < 3; x++)
+    {
+      scanf("%d", &matriz[y][x]);
+
+      if(y<x){
+        soma += matriz[y][x];
+      }
+      if(y>x){
+        produto *= matriz[y][x];
+      }
     }
 
-    printf("\nMatriz 3x3:\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            printf("%d ", matriz[i][j]);
-        }
-        printf("\n");
+  printf("\nMatriz 3x3: \n");
+  for(y = 0; y < 3; y++){
+    for(x = 0; x < 3; x++){
+      printf("%d ", matriz[y][x]);
     }
+    printf("\n");
+  }
 
-    printf("\nSoma dos elementos acima da diagonal principal: %d\n", soma);
-    printf("Produto dos elementos abaixo da diagonal principal: %d\n", produto);
-
-    return 0;
+  printf("\nA Soma dos items acima da diagonal principal e: %d\n", soma);
+  printf("O produto dos items abaixo da diagonal principal e: %d\n", produto);
+  
+  return 0;
 }
